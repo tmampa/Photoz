4531ddf7cfb6617d8c16e4feee0541a15f01366f
tmampa <tmampa94@gmail.com>
2023-11-14 13:56:13 +0200

    Initialize Spring Boot project

---
+++ photoz-backend/src/main/java/com/tmampa/photozbackend/PhotozBackendApplication.java
@@ -0,0 +1,13 @@
+package com.tmampa.photozbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotozBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PhotozBackendApplication.class, args);
+    }
+
+}
---
+++ photoz-backend/src/test/java/com/tmampa/photozbackend/PhotozBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tmampa.photozbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotozBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
